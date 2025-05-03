ae2eb298dfcdd3f63f8defc3526413e32daf1194
OpticsValleyMC <mcintel@aliyun.com>
2025-05-03 10:29:41 +0800

    修改游戏标题，日后可在Faiths.java直接修改版本号

--- src/main/java/net/minecraft/client/Minecraft.java
+++ src/main/java/net/minecraft/client/Minecraft.java
@@ -576,7 +576,7 @@ public class Minecraft implements IThreadListener, IPlayerUsage
     private void createDisplay() throws LWJGLException
     {
         Display.setResizable(true);
-        Display.setTitle("Minecraft 1.8.9");
+        Display.setTitle(Faiths.NAME + " " + Faiths.VERSION);
 
         try
         {
